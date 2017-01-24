294a2eec8bd9fc0f9139cd09aa9e61666e59dfa6
“Aaron <barfootaaron@yahoo.com>
2017-01-24 15:12:22 -0600

    update

--- javascripts/aaron.js
+++ javascripts/aaron.js
@@ -1,1 +1,21 @@
 "use strict";
+
+var Translate = (function(){
+	var german = {
+		"we":"wir",
+		"wish":"wunsch",
+		"you":"sie",
+		"a":"ein",
+		"merry":"frohlich",
+		"christmas":"weihnachten"
+	};
+
+	return {
+		translateToGerman: function (translation) {
+			return german[translation];
+		},
+		translateToGerman: function
+
+
+
+})();
