48cd20de0c745ebc1658161963f061579f96a4f7
Bri Wyatt <mbriwyatt@gmail.com>
2017-01-24 15:15:48 -0600

    modified my js and main js files

--- javascripts/bri.js
+++ javascripts/bri.js
@@ -1,1 +1,20 @@
 "use strict";
+
+var Translate = (function(){
+	var dutch = {
+
+		"merry":"gelukkige",
+		"christmas":"kerst",
+		"and":"en",
+		"happy":"Gelukkig",
+		"new":"Nieu",
+		"year":"wjaar"
+	}
+
+	return {
+	translateToDutch: function(english){
+		return dutch(english);
+	}
+}
+
+}); 
